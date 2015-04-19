45468156635a3cf99e9aefe81052d7691eb8a874
Damon McMinn <damonrmcminn@gmail.com>
2015-04-19 10:16:06 +0100

    Update responsibilities
    Handle validation for DRY.

--- es6/validators/main.js
+++ es6/validators/main.js
@@ -1,16 +1,27 @@
-export default function(fields, schema) {
+export default function(fields, schema, val) {
 
   /**
    * field: {name, fn}
    * schema: {field, type, ....}
    */
 
-  return fields.filter(field => schema[field.name])
+  let failures = fields.filter(field => schema[field.name])
     .map(field => {
       return {
         name: field.name,
         val: schema[field.name],
         fn: field.fn
       }
+    })
+    .filter(validator => !validator.fn(val, validator.val))
+    .map(validator => {
+      return {
+        validator: validator.name,
+        value: validator.val
+      }
     });
+
+  let valid = failures.length === 0;
+
+  return [valid, val, failures];
 }
--- spec/validators/mainSpec.js
+++ spec/validators/mainSpec.js
@@ -3,43 +3,53 @@ var main = require('../../lib/validators/main');
 
 describe('validators/main', function() {
 
-  it('should return a lis', function() {
+  let min = {
+    name: 'min',
+    fn: function(x, min) {
+      return x >= min;
+    }
+  };
 
-    // has been converted to typed schema
+  let max = {
+    name: 'max',
+    fn: function(x, max) {
+      return x <= max
+    }
+  };
+  
+  it('should return a triplet [valid, val, failures]', function() {
+
     let schema = {
       field: 'num',
-      type: 'Number',
       min: 1,
       max: 9
     };
 
-    let fields = [
-      {
-        name: 'min',
-        fn: function(x, min) {
-          return x >= min;
-        }
-      }
-    ];
-    
-    let result = main(fields, schema);
-    let noFields = main([], schema);
-    expect(result.length).toBe(1);
-    expect(result[0].val).toBe(1);
-    expect(noFields.length).toBe(0);
+    let pass = main([min], schema, 2);
+    let fail = main([min, max], schema, 99);
+
+    let noValidators = main([], schema, 10);
+
+    expect(pass[0]).toBe(true);
+    expect(fail[0]).toBe(false);
+
+    expect(fail[2][0].validator).toBe('max');
+
+    expect(noValidators[0]).toBe(true);
+    expect(noValidators[1]).toBe(10);
 
   });
 
-  it('should ignore undeclared fields', function() {
+  it('should ignore undeclared validators', function() {
 
-    // has been converted to typed schema
     let schema = {
-      field: 'num',
-      type: 'Number',
+      field: 'num'
     };
     
-    let result = main([{name: 'min', fn: function() {}}], schema);
-    expect(result.length).toBe(0);
+    let result = main([{name: 'min', fn: function() {}}], schema, 99);
+    expect(result[0]).toBe(true);
+    expect(result[1]).toBe(99);
+    expect(result[2].length).toBe(0);
 
   });
 
